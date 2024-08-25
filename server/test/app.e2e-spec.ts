9545f68033cfdf5f847886618203c1e0817ad81b
omarpr <omarpr@gmail.com>
2024-08-24 23:14:12 -0400

    Improvements to e2e tests and organization.

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -9,6 +9,43 @@ describe('AppController (e2e)', () => {
 
   let accessToken: string;
 
+  const testUser = {
+    email: 'test.user@gmail.com',
+    firstName: 'Test',
+    lastName: 'User',
+    password: 'password',
+  };
+
+  const createUser = (user: {
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    password?: string;
+  }) => request(app.getHttpServer()).post('/users').send(user);
+
+  const loginUser = (credentials: { email?: string; password?: string }) =>
+    request(app.getHttpServer()).post('/auth/login').send(credentials);
+
+  const getProfile = (token?: string) => {
+    const req = request(app.getHttpServer()).get('/auth/profile');
+
+    if (token) {
+      req.set('Authorization', `Bearer ${token}`);
+    }
+
+    return req;
+  };
+
+  const getRoot = (token?: string) => {
+    const req = request(app.getHttpServer()).get('/');
+
+    if (token) {
+      req.set('Authorization', `Bearer ${token}`);
+    }
+
+    return req;
+  };
+
   beforeAll(async () => {
     moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
@@ -18,119 +55,100 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('create user fails if missing fields', () => {
-    request(app.getHttpServer())
-      .post('/users')
-      .send({
-        firstName: 'Test',
-      })
-      .expect(400);
-
-    request(app.getHttpServer())
-      .post('/users')
-      .send({
-        firstName: 'Test',
-        lastName: 'User',
-      })
-      .expect(400);
-
-    request(app.getHttpServer())
-      .post('/users')
-      .send({
-        firstName: 'Test',
-        lastName: 'User',
-        email: 'test.user@gmail.com',
-      })
-      .expect(400);
-  });
-
-  it('create user succeeds if all fields are present', () => {
-    request(app.getHttpServer())
-      .post('/users')
-      .send({
-        email: 'test.user@gmail.com',
-        firstName: 'Test',
-        lastName: 'User',
-        password: 'password',
-      })
-      .expect(200);
-  });
-
-  it('create user fails if email is already taken', () => {
-    request(app.getHttpServer())
-      .post('/users')
-      .send({
-        email: 'test.user@gmail.com',
-        firstName: 'Test',
-        lastName: 'User',
-        password: 'password',
-      })
-      .expect(409);
-  });
-
-  it('login fails if user does not exist', () => {
-    request(app.getHttpServer())
-      .post('/auth/login')
-      .send({
-        email: 'test.bad.user@gmail.com',
-        password: 'password',
-      })
-      .expect(401);
-  });
-
-  it('login fails if password is incorrect', () => {
-    request(app.getHttpServer())
-      .post('/auth/login')
-      .send({
-        email: 'test.user@gmail.com',
-        password: 'bad-password',
-      })
-      .expect(401);
-  });
-
-  it('login succeeds if email and password are correct', () => {
-    request(app.getHttpServer())
-      .post('/auth/login')
-      .send({
-        email: 'test.user@gmail.com',
-        password: 'password',
-      })
-      .expect(200)
-      .expect((res) => {
-        accessToken = res.body.accessToken;
-      });
-  });
-
-  it('get profile fails if not authenticated', () => {
-    request(app.getHttpServer()).get('/auth/profile').expect(401);
-  });
-
-  it('get profile succeeds if authenticated', () => {
-    request(app.getHttpServer())
-      .get('/auth/profile')
-      .set('Authorization', `Bearer ${accessToken}`)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.email).toBe('test.user@gmail.com');
-        expect(res.body.firstName).toBe('Test');
-        expect(res.body.lastName).toBe('User');
-      });
-  });
-
-  it('get / fails if not authenticated', () => {
-    request(app.getHttpServer()).get('/').expect(401);
-  });
-
-  it('get / succeeds if authenticated', () => {
-    request(app.getHttpServer())
-      .get('/')
-      .set('Authorization', `Bearer ${accessToken}`)
-      .expect(200)
-      .expect('Hello Test User!');
-  });
-
   afterAll(async () => {
     await app.close();
     await moduleFixture.close();
+  });
+
+  describe('create user', () => {
+    it('fails if missing fields', () => {
+      const { firstName, lastName, email } = testUser;
+
+      createUser({
+        firstName,
+      }).expect(400);
+
+      createUser({
+        firstName,
+        lastName,
+      }).expect(400);
+
+      createUser({
+        firstName,
+        lastName,
+        email,
+      }).expect(400);
+    });
+
+    it('succeeds if all fields are present', () => {
+      createUser(testUser).expect(200);
+    });
+
+    it('fails if email is already taken', () => {
+      createUser(testUser).expect(409);
+    });
+  });
+
+  describe('login', () => {
+    it('fails if user does not exist', () => {
+      request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+          email: 'test.bad.user@gmail.com',
+          password: testUser.password,
+        })
+        .expect(401);
+    });
+
+    it('fails if password is incorrect', () => {
+      loginUser({
+        email: testUser.email,
+        password: 'bad-password',
+      }).expect(401);
+    });
+
+    it('succeeds if email and password are correct', () => {
+      const { email, password } = testUser;
+
+      loginUser({
+        email,
+        password,
+      })
+        .expect(200)
+        .expect((res) => {
+          accessToken = res.body.accessToken;
+        });
+    });
+  });
+
+  describe('get profile', () => {
+    it('get profile fails if not authenticated', () => {
+      getProfile().expect(401);
+    });
+
+    it('get profile succeeds if authenticated', () => {
+      getProfile(accessToken)
+        .expect(200)
+        .expect((res) => {
+          const { email, firstName, lastName } = testUser;
+
+          expect(res.body.email).toBe(email);
+          expect(res.body.firstName).toBe(firstName);
+          expect(res.body.lastName).toBe(lastName);
+        });
+    });
+  });
+
+  describe('get root', () => {
+    it('fails if not authenticated', () => {
+      getRoot().expect(401);
+    });
+
+    it('succeeds if authenticated', () => {
+      const { firstName, lastName } = testUser;
+      getRoot(accessToken)
+        .expect(200)
+        .expect(`Hello ${firstName} ${lastName}!`);
+    });
   });
 });
